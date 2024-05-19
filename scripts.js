a2ca0766ef63560cf597fd2e4a6b6ce2664fbc3a
Golan Halley <ghalley42@gmail.com>
2024-05-19 15:53:02 -0600

    refactored scripts.js
    cleaned up functions and added global variable.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
+const options = ["rock", "paper", "scissors"];
+
 let getComputerChoice = () => {
-    const options = ["rock", "paper", "scissors"];
     return options[Math.floor( Math.random() * 3 )];
 }
-let testPlayerChoice = (choice) => {
-    return choice == "rock" || choice == "paper" || choice == "scissors";
-}
+let testPlayerChoice = choice => options.some( i => i == choice );
 
 let getPlayerChoice = () => {
-    let quit = 0;
     let playerChoice = prompt("Make your choice");
     while ( playerChoice === null || !testPlayerChoice(playerChoice.toLowerCase()) ){
         playerChoice = prompt("Make a new choice. It must be rock, paper, or scissors.");
@@ -16,32 +14,25 @@ let getPlayerChoice = () => {
 }
 
 let checkWinner = (playerChoice, computerChoice) => {
-    let win;
     switch (playerChoice) {
         case "rock":
-            win = computerChoice == "scissors";
-            break;
+            return win = computerChoice == "scissors";
         case "paper":
-            win = computerChoice == "rock";
-            break;
+            return win = computerChoice == "rock";
         case "scissors":
-            win = computerChoice == "paper";
+            return win = computerChoice == "paper";
     }
-    return win;
 }
 
 let playRound = (playerChoice, computerChoice) => {
-    playerChoice = getPlayerChoice();
-    computerChoice = getComputerChoice();
     console.log(`Player: ${playerChoice}.  Computer: ${computerChoice}.`);
     return playerChoice == computerChoice ? "tie" : checkWinner(playerChoice, computerChoice);
 }
 
 let playGame = () => {
-    let playerScore = 0;
-    let computerScore = 0;
+    let playerScore = 0, computerScore = 0;
     while ( playerScore < 5 && computerScore < 5 ) {
-        let roundScore = playRound();
+        let roundScore = playRound(getPlayerChoice(), getComputerChoice());
         roundScore == "tie" ? console.log("It's a tie!") : roundScore ? playerScore++ : computerScore++;
         console.log(`Player: ${playerScore}. Computer: ${computerScore}.`);
     };
